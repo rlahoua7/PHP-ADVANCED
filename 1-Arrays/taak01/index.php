60a89ea9620cffa876c7b84593629a604ed9fbc9
rlahoua7 <2152116@talnet.nl>
2022-02-04 15:28:43 +0100

    opdracht 1

---
+++ 1-Arrays/taak01/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+$student['naam'] = rayan;
+$Student['achternaam'] = lahoua;
+$student['klas'] = 1C;
+$student['leeftijd'] = 17;
+
+echo "mijn naam is" . $student['naam']; . "rayan";
+#meneer ik weet niet wat er fout aan is.
+?>
+</body>
+</html>
