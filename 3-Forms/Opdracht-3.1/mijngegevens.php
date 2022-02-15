acd8264e2d9d28e272b0c5c44d32d6c9616a30fc
rlahoua7 <2152116@talnet.nl>
2022-02-15 09:52:07 +0100

    opdracht 3.1

---
+++ 3-Forms/Opdracht-3.1/mijngegevens.php
@@ -0,0 +1,19 @@
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <?php
+echo "voornaam is:".$_POST["voornaam"] . "<br>";
+echo "achternaam is:".$_POST["achternaam"] . "<br>";
+echo "leeftijd is:".$_POST["leeftijd"] . "<br>";
+echo "adres is:".$_POST["adres"] . "<br>";
+echo "plaatsnaam is:".$_POST["plaatsnaam"] . "<br>";
+echo "muziekband is:".$_POST["muziekband"];
+?>
+    </body>
+    </html>
