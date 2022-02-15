d1cec78a79d837c82598f7cce201f9934809528c
rlahoua7 <2152116@talnet.nl>
2022-02-15 15:45:50 +0100

    opdracht 3.3

---
+++ 3-Forms/Opdracht-3.3/mijnmuziek.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<form name="invoerformulier"action="mijngegevens.php" method="get">
+        artiest:
+        <input type="text" name="artiest"><br>
+        titel
+        <input type="text" name="titel"><br>
+        uitkomst
+        <input type="text" name="uitkomst"><br>
+        muzieksoort
+        <input type="text" name="muzuieksoort"><br>
+
+        <input type="submit" name="submit" value="verstuur"><br>
+</form>
+
+
+        
+</body>
+</html>
